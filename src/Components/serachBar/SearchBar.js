f379597f3ef3190d140e4da6a3221169913ffcf8
Martín Imbastari <71024069+martinimbastari@users.noreply.github.com>
2023-01-10 17:13:54 -0300

    restore

--- src/Components/serachBar/SearchBar.js
+++ src/Components/serachBar/SearchBar.js
@@ -3,53 +3,6 @@ import styled from 'styled-components'
 import swal from 'sweetalert';
 
 export default function SearchBar() {
-    const [searchInput, setSearchInput] = useState("");
-    
-    const countries = [
-
-  { name: "Belgium", continent: "Europe" },
-  { name: "India", continent: "Asia" },
-  { name: "Bolivia", continent: "South America" },
-  { name: "Ghana", continent: "Africa" },
-  { name: "Japan", continent: "Asia" },
-  { name: "Canada", continent: "North America" },
-  { name: "New Zealand", continent: "Australasia" },
-  { name: "Italy", continent: "Europe" },
-  { name: "South Africa", continent: "Africa" },
-  { name: "China", continent: "Asia" },
-  { name: "Paraguay", continent: "South America" },
-  { name: "Usa", continent: "North America" },
-  { name: "France", continent: "Europe" },
-  { name: "Botswana", continent: "Africa" },
-  { name: "Spain", continent: "Europe" },
-  { name: "Senegal", continent: "Africa" },
-  { name: "Brazil", continent: "South America" },
-  { name: "Denmark", continent: "Europe" },
-  { name: "Mexico", continent: "South America" },
-  { name: "Australia", continent: "Australasia" },
-  { name: "Tanzania", continent: "Africa" },
-  { name: "Bangladesh", continent: "Asia" },
-  { name: "Portugal", continent: "Europe" },
-  { name: "Pakistan", continent"Asia" },
-
-];
-
-    const submitHandler = e => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-        const keyword = e.currentTarget.keyword.value.trim();
-        if(keyword.length === 0) {
-            swal("Tienes que ingresar al menos un producto")
-        } else if (keyword.length < 4){
-            swal("Ingresa mas de 4 caracteres")
-        } else {
-            if (searchInput.length > 0) {
-    countries.filter((country) => {
-    console.log(country.name.match(searchInput))
-});
-}    
-        }
-    }
 
     const Input = styled.input`
     padding: 5px 8px;
