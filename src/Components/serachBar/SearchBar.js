8cdc6154dcb0b3c57a011037d0f00050e46becac
Martín Imbastari <71024069+martinimbastari@users.noreply.github.com>
2023-01-10 17:16:33 -0300

    Componente con logica terminado

--- src/Components/serachBar/SearchBar.js
+++ src/Components/serachBar/SearchBar.js
@@ -1,45 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import swal from 'sweetalert';
 
-export default function SearchBar() {
-
-    const Input = styled.input`
-    padding: 5px 8px;
-    font-size: 16px;
-    border-width: 1px;
-    border-color: #CCCCCC;
-    background-color: #FFFFFF;
-    color: #000000;
-    border-style: hidden;
-    border-radius: 10px;
-    box-shadow: 0px 0px 5px rgba(66,66,66,.75);
-    text-shadow: 0px 0px 5px rgba(66,66,66,.75);
+const Input = styled.input`
+padding: 5px 8px;
+font-size: 16px;
+border-width: 1px;
+border-color: #CCCCCC;
+background-color: #FFFFFF;
+color: #000000;
+border-radius: 10px 0px 0px 10px;
+box-shadow: 0px 0px 5px rgba(66,66,66,.75);
+text-shadow: 0px 0px 5px rgba(66,66,66,.75);
 
 &:focus {
-    outline:none;
+outline:none;
 }
-    `
+`
 
 const Boton = styled.button`
 padding: 6px;
 font-size: 16px;
-border-radius: 0px;
-margin-left: 5px;
+border-radius: 0px 10px 10px 0;
+margin-left: 0px;
 border: none;
 background-color: #2D4263;
 color: white;
 &:hover {
-    background-color: hsl(218, 37%, 20%);
-    cursor: pointer;
+background-color: hsl(218, 37%, 20%);
+cursor: pointer;
 }
 
 `
 
+
+export default function SearchBar( {submitHandler, handleChange, query}) {
+
+    
+    // console.log(Products.filter(product=> product.name.toLowerCase().includes("fe")))
+
+
+
   return (
-    <form onSubmit={submitHandler}>
-        <Input value={searchInput} name='keyword' placeholder='Buscar...'/>
-        <Boton>Buscar</Boton>
+    <form>
+        <Input value={query} onChange={handleChange} name='keyword' placeholder='Buscar...'/>
+        <Boton onClick={() => submitHandler(query)}>Buscar</Boton>
     </form>
   )
 }
+
